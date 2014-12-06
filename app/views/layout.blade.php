012da26022a4c00e4f092098518226e6c549580e
bhoeting <brennan.hoeting@gmail.com>
2014-12-06 14:23:57 -0500

    Set up basic views

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,7 +1,11 @@
-<html>
-	<body>
-		<h1>Laravel</h1>
-
-		@yield('content')
-	</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Music player @yield('title')</title>
+  {{ HTML::style('css/styles.css') }}
+</head>
+<body>
+    @yield('content') 
+</body>
 </html>
